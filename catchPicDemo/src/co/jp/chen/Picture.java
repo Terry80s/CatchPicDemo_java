5616857a7dd54b6d9f75484eb27cd80884683570
TerryChen <chenrukun80s@gmail.com>
2019-04-24 03:24:47 +0900

    Add files via upload

---
+++ catchPicDemo/src/co/jp/chen/Picture.java
@@ -0,0 +1,25 @@
+package co.jp.chen;
+
+public class Picture {
+    private String title;  
+    private String source;  
+    private String upPath;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getUpPath() {
+		return upPath;
+	}
+	public void setUpPath(String upPath) {
+		this.upPath = upPath;
+	} 
+}
